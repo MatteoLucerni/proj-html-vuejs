becac51bb0afcc40e61e1a8d9f78c7a7434ebfe2
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-11 19:42:44 +0200

    added hover effect with dot

--- src/components/baseComps/BaseImagesLine.vue
+++ src/components/baseComps/BaseImagesLine.vue
@@ -2,7 +2,9 @@
 export default {
   props: {
     images: Array,
-    hasBorders: Boolean
+    hasBorders: Boolean,
+    hasDotHover: Boolean
+
   },
   methods: {
     getImagePath(imageName) {
@@ -22,32 +24,77 @@ export default {
 </script>
 
 <template>
-  <section :class="{ 'bordered': hasBorders === true }" class="image-line d-flex">
+  <section :class="{ 'bordered': hasBorders }" class="image-line d-flex">
     <!-- width delle immagini in base al numero di esse -->
-    <img :style="{ width: `calc(100% / ${images.length} - ${compensation}px)` }" v-for="image in images"
-      :src="getImagePath(image.url)" :key="image.desc" :alt="image.desc">
+    <div class="img-cont" :class="{ 'dotHover': hasDotHover }" v-for="image in images"
+      :style="{ width: `calc(100% / ${images.length})` }">
+      <img class="image" :class="{ 'img-bordered': hasBorders }" :src="getImagePath(image.url)" :key="image.desc"
+        :alt="image.desc">
+      <div class="eye-cover">
+        <img src="../../assets/svg/svg-5.svg" alt="eye dot">
+      </div>
+    </div>
   </section>
 </template>
 
 <style lang="scss" scoped>
-img {
-  width: calc(100% / 4);
-}
-
-// solo se se le immagini devono avere il bordo
 .bordered {
   padding: 10px 0;
+  gap: 10px;
+}
+
+.img-bordered,
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.img-cont {
+  position: relative;
+}
+
+.image {
+  transition: opacity .3s;
+  position: relative;
+}
+
+// stile per l'hover dot
+.img-cont.dotHover:hover .image {
+  opacity: 0;
+}
+
+.img-cont.dotHover:hover .eye-cover {
+  opacity: 1;
+}
+
+.eye-cover {
+  position: absolute;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transition: opacity .3s;
+  background: white;
+  background-image: radial-gradient(lightgray 1px, transparent 0);
+  background-size: 8px 8px;
+  background-position: center;
+  width: 100%;
+  height: 100%;
 
   img {
-    margin: 0 6px;
-  }
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    height: 80px;
+    transform: translate(-50%, -50%);
 
-  img:first-child {
-    margin-left: 0;
-  }
-
-  img:last-child {
-    margin-right: 0;
+    &::after {
+      content: "ciao";
+      font-size: 2rem;
+    }
   }
 }
+
+// stile per l'hover card
 </style>
