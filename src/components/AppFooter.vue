13a917fbb741794bf364edb2cea35879d6c08b4e
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-07-12 16:38:48 +0200

    Added AppFooter base structure

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<script>
+import BaseSplitSection from './baseComps/BaseSplitSection.vue'
+export default {
+    components: {
+        BaseSplitSection
+    }
+}
+</script>
+
+<template>
+    <footer>
+        <BaseSplitSection />
+    </footer>
+</template>
+
+<style scoped></style>
